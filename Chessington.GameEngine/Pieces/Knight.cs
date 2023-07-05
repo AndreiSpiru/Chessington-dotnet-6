510d43c99799474a210753e93d9c594379c72396
Spiru Andrei-Tudor <spiru.andrei1@gmail.com>
2023-07-05 15:50:27 +0100

    Added knights

--- Chessington.GameEngine/Pieces/Knight.cs
+++ Chessington.GameEngine/Pieces/Knight.cs
@@ -10,7 +10,24 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            Square currentPosition = board.FindPiece(this);
+            List<Square> possibleMoves = new List<Square>();
+            int[] dx = {1,1,-1,-1};
+            int[] dy = {1,-1,1,-1};
+            for(int i = 0 ; i < 4 ; i ++){
+                int newRow = currentPosition.Row + 1 * dx[i];
+                int newCol = currentPosition.Row + 2 * dy[i];
+                if (0 <= newRow && newRow <= 7 && 0 <= newCol && newCol <=7){
+                    possibleMoves.Add(Square.At(newRow, newCol));
+                }
+
+                newRow = currentPosition.Row + 2 * dx[i];
+                newCol = currentPosition.Row + 1 * dy[i];
+                if (0 <= newRow && newRow <= 7 && 0 <= newCol && newCol <=7){
+                    possibleMoves.Add(Square.At(newRow, newCol));
+                }
+            }
+            return possibleMoves;
         }
     }
 }
