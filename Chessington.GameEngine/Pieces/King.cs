db3b8195b7bf377a05f73e423e2fb7a218a2745f
Spiru Andrei-Tudor <spiru.andrei1@gmail.com>
2023-07-05 15:56:24 +0100

    dded moves for kings

--- Chessington.GameEngine/Pieces/King.cs
+++ Chessington.GameEngine/Pieces/King.cs
@@ -10,7 +10,18 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            Square currentPosition = board.FindPiece(this);
+            List<Square> possibleMoves = new List<Square>();
+            int[] dx = {1,1,1,0,0,-1,-1,-1};
+            int[] dy = {1,-1,0,1,-1,1,-1,0};
+            for(int i = 0 ; i < 8 ; i++){
+                int newRow = currentPosition.Row + dx[i];
+                int newCol = currentPosition.Row + dy[i];
+                if (0 <= newRow && newRow <= 7 && 0 <= newCol && newCol <=7){
+                    possibleMoves.Add(Square.At(newRow, newCol));
+                }
+            }
+            return possibleMoves;
         }
     }
 }
